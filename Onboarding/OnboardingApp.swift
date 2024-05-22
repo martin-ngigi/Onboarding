607025db5db22f84e1f8a7f9dd3f7c8f17b147e7
martin-ngigi <martinwainaina001@gmail.com>
2024-05-22 15:30:21 +0300

    Initial commit

---
+++ Onboarding/OnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingApp.swift
+//  Onboarding
+//
+//  Created by Martin Wainaina on 22/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
